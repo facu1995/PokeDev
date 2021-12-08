01c838dc13dfd8c33fbd382f72dbe9e86926c40b
Jano <janod1300@gmail.com>
2021-12-08 16:54:37 -0300

    Añadi la ruta hacia los datos de pokemons

--- src/components/presentational/CallDataExample.jsx
+++ src/components/presentational/CallDataExample.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import dataPokemon from '../mocks/pokemonData'
+import imagenPoke from '../img/PokeImg/1.png'
+
+
+
+
+
 
 export const CallDataExample = () => {
+    const imagenPokemon = require.context("../img/PokeImg", true);
     return (
         <div>
-            Call data Example
+            <p>Nombre pokemon: {dataPokemon.name}</p>
+            <p>Id Pokemon: {dataPokemon.id}</p>
+            <p>Descripcion</p>
+            <p>Tipos: {dataPokemon.types[0].type.name}</p>
+            <p>Ataque: {dataPokemon.stats[1].base_stat}</p>
+            <p>Defensa: {dataPokemon.stats[2].base_stat}</p>
+            <p>Defensa esp: {dataPokemon.stats[4].base_stat}</p>
+            <p>Speed: {dataPokemon.stats[5].base_stat}</p>
+            <p>HP: {dataPokemon.stats[0].base_stat}</p>
+            <p>Total</p>
+            <p>Location: {dataPokemon.location_area_encounters}</p>
+            <p>evolution xxxxxxxxx</p>
+            
+            
+            <img src={imagenPoke} alt="" />
         </div>
     )
 }
