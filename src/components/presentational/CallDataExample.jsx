042ddd44dffb9783953c338a59ba42e9e99dbce5
arturfrut <77276338+arturfrut@users.noreply.github.com>
2021-12-08 10:47:42 -0300

    Call Data Example creado

---
+++ src/components/presentational/CallDataExample.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CallDataExample = () => {
+    return (
+        <div>
+            Call data Example
+        </div>
+    )
+}
--- src/components/screens/MainScreen.jsx
+++ src/components/screens/MainScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { CallDataExample } from '../presentational/CallDataExample'
 import { ClasesExample } from '../presentational/ClasesExample'
 
 export const MainScreen = () => {
     return (
         <div>
             <ClasesExample />
+            <CallDataExample />
         </div>
     )
 }
