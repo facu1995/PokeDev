74d5da611946289c1b6313a39853b9bf3ac873f3
Facundo <facuunion13@gmail.com>
2021-12-09 11:49:49 -0300

    Acomodar nav

--- src/components/screens/MainScreen.jsx
+++ src/components/screens/MainScreen.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { CallDataExample } from '../presentational/CallDataExample'
 import { ClasesExample } from '../presentational/ClasesExample'
 import { MyPokemonScreen } from './MyPokemonScreen'
-import { PokemonAllScreen } from './PokemonAllScreen'
+
 
 export const MainScreen = () => {
     return (
         <div>
-            <PokemonAllScreen/>
             <ClasesExample />
             <CallDataExample />
             <MyPokemonScreen />
