766bc712c55ae0441f8b58afca83fade51bce880
arturfrut <77276338+arturfrut@users.noreply.github.com>
2021-12-15 13:13:02 -0300

    CardPokemon, imagen y about pasado a responsive

--- src/components/presentational/PokemonScreen/AboutPokemon.jsx
+++ src/components/presentational/PokemonScreen/AboutPokemon.jsx
@@ -9,11 +9,13 @@ const AboutPokemon = ({species} ) => {
         <div className="AboutPokemon">
 
             <CardPokemonAbout />
-            <h5 className='GOLD'>INTRODUCTION</h5>
-            <h2>About Me</h2>
-            <hr className='AboutPokemon__hr'/>
-            <p>{species.flavor_text_entries[0].flavor_text}</p>
-            <p className='sign animate__animated animate__fadeIn '>Im {pokemon.name} </p>
+            <div className='AboutPokemon__flex' >    
+                <h5 className='GOLD'>INTRODUCTION</h5>
+                <h2>About Me</h2>
+                <hr className='AboutPokemon__hr'/>
+                <p>{species.flavor_text_entries[0].flavor_text}</p>
+                <p className='sign animate__animated animate__fadeIn '>Im {pokemon.name} </p>
+            </div>
         </div>
     )
 
