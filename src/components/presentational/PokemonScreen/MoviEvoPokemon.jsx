bb08e1a1aad4e68bc0e2f41f43d4e870a54bb6fa
Facundo <facuunion13@gmail.com>
2021-12-19 20:06:10 -0300

    corregido para pokemon agregado

--- src/components/presentational/PokemonScreen/MoviEvoPokemon.jsx
+++ src/components/presentational/PokemonScreen/MoviEvoPokemon.jsx
@@ -21,52 +21,54 @@ const MoviEvoPokemon = ({ evoluciones, species, setPokemon }) => {
     }
 
     return (
-        <section className='MoviEvoPokemon'>
-            {
-                MoviEvo === 'evo' &&
-                <>
-                    <ul className='MoviEvoPokemon__select'>
-                        <li className='GOLD MoviEvoPokemon__active'>Evolutions</li>
-                        <li onClick={() => { movieEvoState('evo') }}>Moves</li>
-                    </ul>
-                    <div className="MoviEvoPokemon__select__EvolucionesPokemon">
-                        <div className="EvolucionesPokemon__container">
-                            {evoluciones.id.map((el, idx) => {
-                                return <CardPokemonEvoluciones key={idx} name={evoluciones.name[idx]} description={evoluciones.description[idx]} url={'https://pokeapi.co/api/v2/pokemon/' + el} id={pokemon.id} />
-                            })}
-                        </div>
-                    </div>
-                </>
-            }
-            {
-                MoviEvo === 'mov' &&
-                <>
-                    <ul className='MoviEvoPokemon__select'>
-                        <li onClick={() => { movieEvoState('mov') }}>Evolutions</li>
-                        <li className='GOLD MoviEvoPokemon__active'>Moves</li>
-                    </ul>
-                    <div className="MoviEvoPokemon__Movimientos">
-                        {agregarMoves === false && <>
-                            <ul className='MoviEvoPokemon__Movimientos__bar'>
-                                <li className='flex-centerAll'>ID </li>
-                                <li className='flex-centerAll'> &nbsp; &nbsp;NAME</li>
-                                <li className='flex-centerAll'>TYPE</li>
-                                <li className='flex-centerAll'>POWER</li>
-
-                            </ul>
-                            <div className='MoviEvoPokemon__Movimientos__list'>
-                                <MostrarMovimientoPokemon movesAll={pokemon.moves} EliminarPoder={EliminarPoder} />
+        <>{!pokemon.new &&
+            <section className='MoviEvoPokemon'>
+                {
+                    MoviEvo === 'evo' &&
+                    <>
+                        <ul className='MoviEvoPokemon__select'>
+                            <li className='GOLD MoviEvoPokemon__active'>Evolutions</li>
+                            <li onClick={() => { movieEvoState('evo') }}>Moves</li>
+                        </ul>
+                        <div className="MoviEvoPokemon__select__EvolucionesPokemon">
+                            <div className="EvolucionesPokemon__container">
+                                {evoluciones.id.map((el, idx) => {
+                                    return <CardPokemonEvoluciones key={idx} name={evoluciones.name[idx]} description={evoluciones.description[idx]} url={'https://pokeapi.co/api/v2/pokemon/' + el} id={pokemon.id} />
+                                })}
                             </div>
-                            <div className='MoviEvoPokemon__Movimientos__barBottom'></div>
-                            {/* <button className='btn btn-form' onClick={()=>setAgregarMoves(true)} >Agregar Movimientos</button> */}
-                        </>}
-                        {agregarMoves === true && <><AddMoveForm setPokemon={setPokemon} />
-                        <button className="btn btn-form" type="submit" onClick={() =>{setAgregarMoves(false)}}>Atras</button></>}
-                    </div>
+                        </div>
+                    </>
+                }
+                {
+                    MoviEvo === 'mov' &&
+                    <>
+                        <ul className='MoviEvoPokemon__select'>
+                            <li onClick={() => { movieEvoState('mov') }}>Evolutions</li>
+                            <li className='GOLD MoviEvoPokemon__active'>Moves</li>
+                        </ul>
+                        <div className="MoviEvoPokemon__Movimientos">
+                            {agregarMoves === false && <>
+                                <ul className='MoviEvoPokemon__Movimientos__bar'>
+                                    <li className='flex-centerAll'>ID </li>
+                                    <li className='flex-centerAll'> &nbsp; &nbsp;NAME</li>
+                                    <li className='flex-centerAll'>TYPE</li>
+                                    <li className='flex-centerAll'>POWER</li>
 
-                </>
-            }
-        </section>
+                                </ul>
+                                <div className='MoviEvoPokemon__Movimientos__list'>
+                                    <MostrarMovimientoPokemon movesAll={pokemon.moves} EliminarPoder={EliminarPoder} />
+                                </div>
+                                <div className='MoviEvoPokemon__Movimientos__barBottom'></div>
+                                {/* <button className='btn btn-form' onClick={()=>setAgregarMoves(true)} >Agregar Movimientos</button> */}
+                            </>}
+                            {agregarMoves === true && <><AddMoveForm setPokemon={setPokemon} />
+                                <button className="btn btn-form" type="submit" onClick={() => { setAgregarMoves(false) }}>Atras</button></>}
+                        </div>
+
+                    </>
+                }
+            </section>
+        }</>
     )
 }
 
--- src/components/presentational/PokemonScreen/StatsPokemon.jsx
+++ src/components/presentational/PokemonScreen/StatsPokemon.jsx
@@ -4,10 +4,10 @@ import UsePokemon from "../../context/usePokemon";
 const StatsPokemon = () => {
     const pokemon = UsePokemon();
 
-    const sumarStats =() =>{
+    const sumarStats = () => {
         let total = 0;
-         pokemon.stats.forEach(stat => {
-            total += stat.base_stat
+        pokemon.stats.forEach(stat => {
+            total += parseInt(stat.base_stat);
         })
         return total;
     }
@@ -21,23 +21,23 @@ const StatsPokemon = () => {
         });
         return may
     }
-        
 
-     let statBar = pokemon.stats.map((stat,idx)=>
-         <div key={idx} className='StatsPokemon__statBar' style={{width: `${stat.base_stat * 100 / greaterStat()}%`}}>{/* {greaterStat()} */}</div> 
-     )
+
+    let statBar = pokemon.stats.map((stat, idx) =>
+        <div key={idx} className='StatsPokemon__statBar' style={{ width: `${stat.base_stat * 100 / greaterStat()}%` }}>{/* {greaterStat()} */}</div>
+    )
 
 
     return (
         <div className="StatsPokemon">
-{/* 
+            {/* 
             <h5 className='GOLD'>SOME STADISTICS</h5> */}
             <h2>My Stats</h2>
-            <hr className='AboutPokemon__hr'/>
-            <p>I'm a Freelancer Pokemon, living in the wild. Waiting for someone like you, who wants to be a pokemon master, also, i want to meet new people and win some experience, and maybe if we train a lot, EVOLVE! I don't have degrees or things like that, but i can show you my stats where you can see my true potential!   </p>            
-    
+            <hr className='AboutPokemon__hr' />
+            <p>I'm a Freelancer Pokemon, living in the wild. Waiting for someone like you, who wants to be a pokemon master, also, i want to meet new people and win some experience, and maybe if we train a lot, EVOLVE! I don't have degrees or things like that, but i can show you my stats where you can see my true potential!   </p>
+
             <div className='StatsPokemon__stats'>
-          
+
                 <ul className='StatsPokemon__stats_left'>
                     <li>HP:</li>
                     <li>ATTACK</li>
@@ -49,7 +49,7 @@ const StatsPokemon = () => {
                 </ul>
                 <div className='StatsPokemon__stats_center'>
                     {statBar}
-                </div> 
+                </div>
                 <ul className='StatsPokemon__stats_right' >
                     <li>{pokemon.stats[0].base_stat}</li>
                     <li>{pokemon.stats[1].base_stat}</li>
